acf258ee010740336e92a5c877a4b1822bffea97
Philipp Ossler <philipp.ossler@gmail.com>
2019-04-05 07:51:33 +0200

    extend model

--- src/main/kotlin/io/zeebe/voyager/model/Workflow.kt
+++ src/main/kotlin/io/zeebe/voyager/model/Workflow.kt
@@ -1,4 +1,38 @@
 package io.zeebe.voyager.model
 
-class Workflow(val bpmnProcessId: String) {
-}
+class Workflow(
+	val bpmnProcessId: String,
+	val startEvent: StartEvent
+)
+
+open class FlowNode(
+	val id: String,
+	val incomming: List<SequenceFlow>,
+	val outcomming: List<SequenceFlow>
+)
+
+class SequenceFlow(
+	val id: String,
+	val source: FlowNode,
+	val target: FlowNode
+)
+
+class StartEvent(
+	id: String,
+	outcomming: List<SequenceFlow>
+) : FlowNode(
+	id = id,
+	incomming = emptyList(),
+	outcomming = outcomming
+)
+
+class Task(
+	id: String,
+	incomming: List<SequenceFlow>,
+	outcomming: List<SequenceFlow>
+) : FlowNode(
+	id = id,
+	incomming = incomming,
+	outcomming = outcomming
+)
+				
--- src/main/kotlin/io/zeebe/voyager/transformer/WorkflowTransformer.kt
+++ src/main/kotlin/io/zeebe/voyager/transformer/WorkflowTransformer.kt
@@ -2,20 +2,32 @@ package io.zeebe.voyager.transformer
 
 import io.zeebe.voyager.model.Workflow
 import io.zeebe.model.bpmn.BpmnModelInstance
-import io.zeebe.model.bpmn.instance.Process
+import io.zeebe.model.bpmn.instance.Process as BpmnProcess
+import io.zeebe.model.bpmn.instance.StartEvent as BpmnStartEvent
+import io.zeebe.voyager.model.StartEvent
 
 object WorkflowTransformer {
-	
+
 	fun transform(model: BpmnModelInstance): List<Workflow> {
-		val processes = model.getModelElementsByType(Process::class.java)
-		
+		val processes = model.getModelElementsByType(BpmnProcess::class.java)
+
 		val workflows = processes.map { process ->
- 			val bpmnProcessId = process.getId()
-			
-			Workflow(bpmnProcessId = bpmnProcessId)
+			val bpmnProcessId = process.getId()
+
+			val startEvents = process.getChildElementsByType(BpmnStartEvent::class.java)
+
+			val startEvent = startEvents.toList().get(0)
+
+			Workflow(
+				bpmnProcessId = bpmnProcessId,
+				startEvent = StartEvent(
+					id = startEvent.getId(),
+					outcomming = emptyList()
+				)
+			)
 		}
-		
+
 		return workflows;
 	}
-	
+
 }
